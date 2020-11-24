3416e811eaf284c07d3aa29a2d14b44cb2ecab2c
avrilquach <family.happy89@gmail.com>
2020-11-24 09:11:23 -0700

    nhanxetsanpham

--- js/main.js
+++ js/main.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
  $(".do-nicescrol").niceScroll({
 	autohidemode: false, 
  });
+ $(".san_pham_da_xem_2 .write").click(function(e){
+	 $(".bgpopup").show();
+	 $(".popup").show();
+ })
+ $(".popup .close_popup").click(function(e){
+	$(".bgpopup").hide();
+	$(".popup").hide();
+ })
+ $(".bgpopup").click(function(e){
+	$(".bgpopup").hide();
+	$(".popup").hide();
+ })
 })
