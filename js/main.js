29cf34729ea275a1e37aff7379adf0a9f89ace15
avrilquach <family.happy89@gmail.com>
2020-11-28 08:35:33 -0700

    thanhtoan2

--- js/main.js
+++ js/main.js
@@ -23,4 +23,31 @@ $(document).ready(function () {
 $(".ghichudonhang .box").click(function(e){
 	$(this).toggleClass("active");
 })
+$(".diachinhanhang .boxship2 .style a").click(function(e){
+	$(".bgpopup").show();
+	$(".addaddress").show();
+})
+$(".addaddress .close_popup").click(function(e){
+ $(".bgpopup").hide();
+ $(".addaddress").hide();
+})
+$(".bgpopup").click(function(e){
+ $(".bgpopup").hide();
+ $(".addaddress").hide();
+})
+$(".choose").click(function(e){
+	$(this).parent().toggleClass("active");
+})
+$(".chinhsua").click(function(e){
+	$(".bgpopup").show();
+	$(".editaddress").show();
+})
+$(".editaddress .close_popup").click(function(e){
+ $(".bgpopup").hide();
+ $(".editaddress").hide();
+})
+$(".bgpopup").click(function(e){
+ $(".bgpopup").hide();
+ $(".editaddress").hide();
+})
 })
